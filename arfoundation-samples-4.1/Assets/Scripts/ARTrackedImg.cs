697504e3e1cacd3a3b3b367861733f029e6e238e
harrietPark <29harriet@naver.com>
2021-11-23 12:39:58 -0900

    multi_image_tracking

---
+++ arfoundation-samples-4.1/Assets/Scripts/ARTrackedImg.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+public class ARTrackedImg : MonoBehaviour
+{
+    public ARTrackedImageManager trackedImageManager;
+    public List<GameObject> objectList = new List<GameObject>();
+    private Dictionary<string, GameObject> prefabDic = new Dictionary<string, GameObject>();
+
+    private void Awake()
+    {
+        foreach (GameObject obj in objectList)
+        {
+            string tName = obj.name;
+            prefabDic.Add(tName, obj);
+        }
+    }
+
+    private void OnEnable()
+    {
+        trackedImageManager.trackedImagesChanged += ImageChanged;
+    }
+    private void OnDisable()
+    {
+        trackedImageManager.trackedImagesChanged -= ImageChanged;
+    }
+
+    private void ImageChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (ARTrackedImage trackedImage in eventArgs.added)
+        {
+            UpdateImage(trackedImage);
+        }
+        foreach (ARTrackedImage trackedImage in eventArgs.updated)
+        {
+            UpdateImage(trackedImage);
+        }
+    }
+
+    private void UpdateImage(ARTrackedImage trackedImage)
+    {
+        string name = trackedImage.referenceImage.name;
+        GameObject tObj = prefabDic[name];
+        tObj.transform.position = trackedImage.transform.position;
+        tObj.transform.rotation = trackedImage.transform.rotation;
+        tObj.SetActive(true);
+    }
+}
