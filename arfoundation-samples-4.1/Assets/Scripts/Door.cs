db0cab5ebb0fa300475c9314d6dd62fd31c2d3ab
harrietPark <29harriet@naver.com>
2022-01-12 17:41:07 -0900

    four

--- arfoundation-samples-4.1/Assets/Scripts/Door.cs
+++ arfoundation-samples-4.1/Assets/Scripts/Door.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Door : Interactable
 {
-    public Animator animator;
-    public ARTapToPlaceObject arTapToPlaceObject;
+    public Animator animator; 
+    
     public override void OnTouch()
     {
         Debug.Log("Door touched");
         animator.SetTrigger("isOpen");
-        arTapToPlaceObject.enabled = false;
     }
 }
--- arfoundation-samples-4.1/Assets/Scripts/DoorInstantiate.cs
+++ arfoundation-samples-4.1/Assets/Scripts/DoorInstantiate.cs
@@ -1,19 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class DoorInstantiate : MonoBehaviour
 {
     public GameObject door;
+    public Animator doorAnimator;
+    private ARPlaneManager arPlaneManager;
+    private ARTapToPlaceObject tapToPlaceObject;
+
     private Vector3 openedScale = new Vector3(100, 100, 100);
     private Vector3 closedScale = new Vector3(100, 0, 100);
 
+    public bool isInstantiated = false;
     public float T = 0;
     public float decayPerSecond;
+
+    private void Awake()
+    {
+        arPlaneManager = FindObjectOfType<ARPlaneManager>();
+        tapToPlaceObject = FindObjectOfType<ARTapToPlaceObject>();
+    }
     void Update()
     {
         T += decayPerSecond * Time.deltaTime;
         T = Mathf.Clamp01(T);
         door.transform.localScale = Vector3.Lerp(closedScale, openedScale, T);
+        if (T >= 1) isInstantiated = true;
+
+        if (isInstantiated)
+        {
+            doorAnimator.enabled = true;
+            arPlaneManager.enabled = false;
+            tapToPlaceObject.enabled = false;
+            
+        }
     }
+
 }
