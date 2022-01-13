892c82e202c2afda0f1dda173bf11a1478ba6905
harrietPark <29harriet@naver.com>
2022-01-12 15:00:57 -0900

    second

--- arfoundation-samples-4.1/Assets/Scripts/Door.cs
+++ arfoundation-samples-4.1/Assets/Scripts/Door.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Door : Interactable
 {
     public Animator animator;
+    public ARTapToPlaceObject arTapToPlaceObject;
     public override void OnTouch()
     {
         Debug.Log("Door touched");
         animator.SetTrigger("isOpen");
+        arTapToPlaceObject.enabled = false;
     }
 }
